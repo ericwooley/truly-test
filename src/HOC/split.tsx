36a1b46fd5bd237a26fcd4dc880564547e7dc634
Eric Wooley <ericwooley@gmail.com>
2017-11-18 13:53:10 -0800

    clean up code splitter

--- src/HOC/split.tsx
+++ src/HOC/split.tsx
@@ -1,9 +1,16 @@
 import * as React from "react";
 import CircularProgress from "material-ui/CircularProgress";
+import Card from "material-ui/Card/Card";
 /**
  * 
- * @param moduleLoader A react component to render
+ * @param moduleLoader A react component to render a loading circle while the split module is loading.
  */
+// TODO: This style needs help, but good enough for now
+const loaderWrapperStyle = {
+  textAlign: "center",
+  paddingBottom: 100
+
+};
 export default function LoaderHOC <P>(moduleLoader: () => Promise<React.ComponentClass<P>> ) {
   class LoaderWrapper extends React.Component<P> {
     state: {Component: React.ComponentClass<P>|null} = {
@@ -15,7 +22,13 @@ export default function LoaderHOC <P>(moduleLoader: () => Promise<React.Componen
     }
     render () {
       const Component = this.state.Component;
-      return Component ? <Component {...this.props} /> : <CircularProgress style={{marginTop: 100}} />;
+      return Component ? 
+      <Component {...this.props} /> : 
+      (
+        <Card style={loaderWrapperStyle as any}>
+          <CircularProgress style={{marginTop: 100}} />
+        </Card>
+      );
     }
   }
   return LoaderWrapper;
